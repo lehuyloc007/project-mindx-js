7eba871a5f1d1219003baccfc4a0dd964971d1b4
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2021-10-09 22:29:42 +0700

    add file

---
+++ index.js
@@ -0,0 +1,15 @@
+import { CommonJsCreateEl, CommonJsAddClass, CommonJsRemoveClass } from "./components/shared/common.js";
+
+const app = document.getElementById("app");
+const hello = CommonJsCreateEl("h1")
+hello.innerHTML = "Hello World";
+CommonJsAddClass(hello, "red", "blue");
+CommonJsRemoveClass(hello, "red");
+
+
+const setScreen = ($container) => {
+    app.appendChild($container);
+}
+
+setScreen(hello);
+export { setScreen };
