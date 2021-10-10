55ea06e238c4d1ed219a0614831e2d7c0609f2a8
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2021-10-10 11:00:57 +0700

    xssss

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ hello.innerHTML = "Hello World";
 CommonJsAddClass(hello, "red", "blue");
 CommonJsRemoveClass(hello, "red");
 
-
+const a = 1
 const setScreen = ($container) => {
     app.appendChild($container);
 }
